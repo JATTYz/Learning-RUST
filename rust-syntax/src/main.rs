38528ac8094703fccf6a00eb5e0e9bcc383ebb65
Jattyz <jaturon.tepjuk@gmail.com>
2023-03-06 23:24:40 +1100

    delete commemted codes

--- rust-syntax/src/main.rs
+++ rust-syntax/src/main.rs
@@ -14,10 +14,6 @@ fn main() {
     let mut s = String::from("hello");
     let borrow = s.get(0..5);
     borrow_string(&s);
-    // borrow_string(&mut s);
-    // borrow_string(&mut s);
-    // let number: u32 = 123;
-    // use_number(number);
     use_string(&s);
     print!("{}", s);
 }
